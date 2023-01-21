b9cb1f6ffc80f38f7607790eff68dedabd1215f5
Angelo <angelofag3095@gmail.com>
2023-01-21 12:51:04 +0100

    Prova

--- src/main/java/it/developer/film/FilmApplication.java
+++ src/main/java/it/developer/film/FilmApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 public class FilmApplication {
 
 	public static void main(String[] args) {
+		System.out.println("BENVENUTI");
 		SpringApplication.run(FilmApplication.class, args);
 	}
 
