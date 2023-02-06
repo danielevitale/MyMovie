59f2bdad1a01f26ab600d9a3636dd9a749cde60a
danielevitale <dansa030@gmail.com>
2023-02-06 22:06:28 +0100

    Inizio PdfController e PdfService

---
+++ src/main/java/it/developer/film/controller/PdfController.java
@@ -0,0 +1,69 @@
+package it.developer.film.controller;
+
+import it.developer.film.entity.Movie;
+import it.developer.film.service.MovieService;
+import it.developer.film.service.PdfService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.InputStream;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/pdf")
+public class PdfController {
+
+    @Autowired
+    MovieService movieService;
+    @Autowired
+    PdfService pdfService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> createPdf(@PathVariable long id) {
+
+        Optional<Movie> m = movieService.findById(id);
+        if (!m.isPresent())
+            return new ResponseEntity<String>("Post not found", HttpStatus.NOT_FOUND);
+
+        InputStream pdfFile = null;
+        ResponseEntity<InputStreamResource> responseEntity = null;
+
+        try {
+            pdfFile = pdfService.createPdfFromPost(m.get());
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.parseMediaType("application/pdf"));
+            headers.add("Access-Control-Allow-Origin", "*");
+            headers.add("Access-Control-Allow-Method", "GET");
+            headers.add("Access-Control-Allow-Header", "Content-Type");
+            headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+            headers.add("Pragma", "no-cache");
+            headers.add("Expires", "0");
+            headers.add("Content-disposition", "inline; filename=" + m.get().getId() + ".pdf");
+
+            responseEntity = new ResponseEntity<InputStreamResource>(
+                    new InputStreamResource(pdfFile),
+                    headers,
+                    HttpStatus.OK
+            );
+
+        } catch (Exception ex) {
+            responseEntity = new ResponseEntity<InputStreamResource>(
+                    new InputStreamResource(null, "Error creating PDF"),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+
+        return responseEntity;
+
+    }
+
+
+}
---
+++ src/main/java/it/developer/film/service/PdfService.java
@@ -0,0 +1,87 @@
+package it.developer.film.service;
+
+import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.io.image.ImageData;
+import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.kernel.colors.Color;
+import com.itextpdf.kernel.colors.ColorConstants;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.borders.SolidBorder;
+import com.itextpdf.layout.element.*;
+import com.itextpdf.layout.properties.HorizontalAlignment;
+import com.itextpdf.layout.properties.TabAlignment;
+import com.itextpdf.layout.properties.TextAlignment;
+import it.developer.film.entity.Movie;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.css.RGBColor;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+@Service
+public class PdfService {
+
+    @Value("${movie.path}")
+    private String pathImage;
+
+    public InputStream createPdfFromPost(Movie m) throws IOException {
+
+        String title = m.getTitle().toUpperCase();
+        String plot = m.getPlot();
+        String productionYear = String.valueOf(m.getProductionYear().getYear()); // (yyyy-MM-dd)
+        String image = pathImage.concat(m.getPoster());
+        ImageData data = ImageDataFactory.create(image);
+        Image img = new Image(data);
+
+        String duration = "Durata: ".concat(String.valueOf(m.getDuration()));
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        // GENERAZIONE PDF
+        // creazione pdf vuoto e impostazioni di pagina
+        PdfDocument pdf = new PdfDocument(new PdfWriter(out));
+        Document document = new Document(pdf, PageSize.A4); // Il formato A4 corrisponde a (595.0F, 842.0F).
+        // the 595.0F - the x coordinate of lower left point
+        // the 842.0F - the y coordinate of lower left point
+
+        // title, image, content, updatedAt (yyyy-MM-dd), author (username)
+        // ...
+        Text text = new Text(title);
+        Paragraph paraTitle = new Paragraph().setTextAlignment(TextAlignment.CENTER).setMarginBottom(10);
+        paraTitle.add(text.setBold().setFontColor(ColorConstants.RED));
+
+
+        img.setHeight(400).setWidth(PageSize.A4.getWidth()/2);
+        img.setMarginLeft((PageSize.A4.getWidth()/4)-document.getLeftMargin());
+
+        Paragraph paraPlot = new Paragraph(plot).setBorder(new SolidBorder(1)).setPadding(10);;
+
+        Paragraph paraDuration = new Paragraph (duration);
+        Paragraph paraProduYear = new Paragraph (productionYear);
+
+
+
+        document.add(paraTitle);
+        document.add(img);
+        document.add(paraPlot);
+        document.add(paraDuration);
+        document.add(paraProduYear);
+
+        // CHIUSURA DOCUMENTO
+        document.close();
+
+        InputStream in = new ByteArrayInputStream(out.toByteArray());
+
+        return in;
+    }
+}
